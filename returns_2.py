84689fecdcd9b5bf14823236d5232b5681aee994
David <david-adeniji@hotmail.co.uk>
2021-12-09 07:10:51 +0000

    Native currency average

--- returns_2.py
+++ returns_2.py
@@ -280,8 +280,8 @@ def returns():
     
         ## Watchlist
         if df.empty:
-            holdings_dict[symbol]['Current Holdings'] = 0
-            holdings_dict[symbol]['Current Average'] = 0.0
+            holdings_dict[symbol]['Current Holdings GBP'] = 0
+            holdings_dict[symbol]['Current Average GBP'] = 0.0
         
         else:
             print(f'------- {symbol} History -------')
@@ -324,19 +324,153 @@ def returns():
                             ## However average stays the same
                             c -= s*average
               
-            holdings_dict[symbol]['Current Holdings'] = holdings #formatting(holdings)
-            holdings_dict[symbol]['Current Average'] = average #formatting(average)
+            holdings_dict[symbol]['Current Holdings GBP'] = holdings #formatting(holdings)
+            holdings_dict[symbol]['Current Average GBP'] = average #formatting(average)
                 
-            print(f'Holdings Average: {holdings} @ {average}')  
+            print(f'Holdings Average: {holdings} @ {average}')
+    
+    # Native currency averages
+    
+    for symbol in all_holdings['Ticker Symbol'].tolist():
+        
+        #symbol = '3LTS'
+                
+        df = trades[trades['Ticker Symbol'] == symbol]
+        
+        df = df.reset_index().drop('index', axis=1)
+        
+        # formatting float to resolve floating point Arithmetic Issue
+        # https://www.codegrepper.com/code-examples/delphi/floating+point+precision+in+python+format
+        # https://docs.python.org/3/tutorial/floatingpoint.html
+        # could also use the math.isclose() function
+        df['Shares'] = df['Shares'].apply(lambda x: float("{:.6f}".format(x))) # Trading 212 only allows fraction of shares up to 6dp 
+    
+        averages = []
+        quantities = []
+
+        ## Depricated: don't have a watchlist anymore
+        if df.empty:
+            holdings_dict[symbol]['Current Holdings'] = 0
+            holdings_dict[symbol]['Current Average'] = 0.0
+        
+        else:
+            print(f'------- {symbol} History -------')
+            
+            # for ii, row in df.iterrows():
+                
+            ## currently does not take into account fees 
+            ## should use total cost column instead later
+            ## trading212 doesn't include fees in returns per stock
+            
+            # share_lis = df['Shares'][:ii+1].tolist()
+            # price_lis = df['Price'][:ii+1].tolist()
+            # type_lis = df['Type'][:ii+1].tolist()
+            # fx_lis = df['Exchange rate'][:ii+1].tolist()
+            # ex_lis = df['Execution_Price'][:ii+1].tolist()
+            
+            # Price list is GBP, Execution_Price is the stock's currency
+            
+            share_lis = df['Shares'].tolist()
+            price_lis = df['Price'].tolist()
+            type_lis = df['Type'].tolist()
+            fx_lis = df['Exchange rate'].tolist()
+            ex_lis = df['Execution_Price'].tolist()
+        
+            c = holdings = average = 0
+            #= x
+                    
+            # below basically same as 'for ii, row in df.iterrows():'
+                        
+            for s, p, t, fx, ex in list(zip(share_lis, price_lis, type_lis, fx_lis, ex_lis)):
+                
+                if t == 'Buy':
+                    c += s*p
+                    holdings += s
+                
+                    # The average cost price is calculated by reference to purchases only; it makes no reference to sales.
+                    average = ex
+                    
+                    # if fx != 0.01:
+                    #     average = p * (1/fx)
+                    # else:
+                    #     average = c / holdings
+
+                    averages.append(average)
+                    quantities.append(s)
+
+                    sums = [a*b for a,b in zip(averages, quantities)]
+        
+                    if len(quantities) == 0:
+                        average = 0
+                    else:
+                        average = sum(sums) / sum(quantities) # could aslo use holdings
+                
+                    print(f'Buy Order: {s} @ {ex}')
+                    print(f'New Holdings Average: {holdings} @ {average}')
+                    print('--------------')
+                
+                else:
+    
+                    holdings -= s 
+                    print(f'Sell Order: {s} @ {ex}')
+                    
+                    if holdings == 0:
+                        ## Reset average after liquidating stock
+                        average = 0
+                        c = 0
+                        
+                        averages = []
+                        quantities = []
+                        
+                        print('Sold all holdings')
+                    else:
+                        
+                        print(f'New Holdings Average: {holdings} @ {average}')
+                        
+                        ## Take away shares from from holding average
+                        ## However average stays the same
+                        
+                        averages = []
+                        quantities = []
+                        #averages_gbp = []
+
+                        averages.append(average)
+                        quantities.append(holdings)
+
+                        c -= s*average
+                    
+                    print('--------------')
+                
+        # sums = [a*b for a,b in zip(averages, quantities)]
+        
+        # if len(quantities) == 0:
+        #     average = 0
+        # else:
+        #     average = sum(sums) / sum(quantities) # could aslo use holdings
+        
+        holdings_dict[symbol]['Current Holdings'] = holdings #formatting(holdings)
+        holdings_dict[symbol]['PRICE'] = average #formatting(average)
+        holdings_dict[symbol]['YF_TICKER'] = df['YF_TICKER'].iloc[0]
+        holdings_dict[symbol]['Sector'] = df['Sector'].iloc[0]
+        holdings_dict[symbol]['Industry'] = df['Industry'].iloc[0]
+
     
     holdings_df = pd.DataFrame.from_dict(holdings_dict, orient='index').reset_index().rename(columns={'index':'Ticker Symbol'})
     returns_df = pd.DataFrame.from_dict(returns_dict, orient='index').reset_index().rename(columns={'index':'Date'})
 
     returns_df['Date'] = pd.to_datetime(returns_df['Date'], format='%d-%m-%Y')
     
+    active = holdings_df[holdings_df['Current Holdings'] > 0.000002]
+    active['PREV_CLOSE'] = ''
+
+    active.rename(columns={'Ticker Symbol':'Ticker'}, inplace=True)
+
+    active['CAPITAL'] = active['Current Holdings'] * active['Current Average GBP']
+
     #returns_df = pd.DataFrame(holdings_dict).transpose().reset_index(level=0).rename(columns={'index':'Ticker Symbol'})
     returns_df.to_sql('returns', engine, if_exists='replace')
     holdings_df.to_sql('holdinigs', engine, if_exists='replace')
+    active.to_sql('portfolio', engine, if_exists='replace')
     
     trades.drop(columns=['index'], inplace=True)
     trades.sort_values(['Trading day','Trading time'], inplace=True, ascending=True)
